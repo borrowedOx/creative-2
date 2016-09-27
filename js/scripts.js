c1d50a94335895cbb85477484cde63207a1a1cde
Jonathan Oxborrow <jon.oxbor@gmail.com>
2016-09-27 13:35:17 +0000

    markers work and can add text/title to them

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,11 @@
-$(document).ready(function() {
-	$("#get_data").on("click", function () {
-        var param = $("#param").val();
-        console.log(param);
-
-        var url = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=jonathan-testing-PRD-82f530923-d8958cf2&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=" +param;
-        $.getJSON(url, function(data) {
-        var everything;
-        everything = "<ul>";
-	everything += "</ul>";
-	console.log(data.toString());
-        $('#results').html(everything);
-})
-
-
-});
-
-});
-
-
-var map;
+var state = false;
 var lat = 0;
 var lon = 0;
+var markers = [];
+var latlon={};
 function initMap() {
-	//lat = 0;
-	//lon = 0;
 
 	loc();
-
-
-	console.log(lat);
-	console.log(lon);
-       
-
-
 
       }
 
@@ -48,11 +21,46 @@ function loc() {
 		console.log(lat);
 		console.log(lon);
 	console.log("hi");
- map = new google.maps.Map(document.getElementById('map'), {
+var  map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: lat, lng: lon},
-          zoom: 18
+          zoom: 12
         });
 
+
+	
+
+	map.addListener('click', function(e) {
+		latlon = e.latLng;
+		var data = "<div id='pop_out'>";
+		data += $("#data_text").val();
+		data += "</div>";
+		$("#data_text").val("");		
+		var title = $("#title_text").val();
+		$("#title_text").val("");		
+
+
+		var marker = new google.maps.Marker({
+			position: latlon,
+			map:map,
+			title: title
+		});
+
+	var infowindow = new google.maps.InfoWindow({
+		content: data
+	});
+
+	marker.addListener('click', function() {
+		infowindow.open(map, marker);
+		console.log("marker clicked");
+	
+
+	});
+
+		markers.push(marker);
+
+	
+		console.log(e.latLng);
+	});
 
 	})
 }
